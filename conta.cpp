6ce555ffdd7d28b0592a9771c22f21c2a8c0f6c7
BFDellorto <47565428+BFDellorto@users.noreply.github.com>
2020-07-15 21:23:56 -0300

    Add files via upload

--- conta.cpp
+++ conta.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <typeinfo>
 using namespace std;
 
 class Cliente{
@@ -25,6 +27,56 @@ void Cliente::setCPF(int novo_cpf){
 int Cliente::getCPF(){
         return this->cpf;
 }
+
+class ClientePJ{
+        public:
+        int cnpj;
+        string nome;
+
+        void setNome(string novo_nome);
+        string getNome();
+        void setCNPJ(int novo_cnpj);
+        int getCNPJ();
+
+};
+
+void ClientePJ::setNome(string novo_nome){
+        this->nome = novo_nome;
+}
+string ClientePJ::getNome(){
+        return this->nome;
+}
+void ClientePJ::setCNPJ(int novo_cnpj){
+        this->cnpj = novo_cnpj;
+}
+int ClientePJ::getCNPJ(){
+        return this->cnpj;
+}
+
+template <class C>
+class Fila{
+        public:
+        vector<C> pessoa;
+
+        void getClientes();
+        void setClientes(C newPessoa);
+};
+
+template<> void Fila<Cliente>::getClientes(){
+        for(int i=0; i<pessoa.size(); i++){
+                cout << this->pessoa[i].getNome() << endl;
+        }
+}
+template<> void Fila<ClientePJ>::getClientes(){
+        for(int i=0; i<pessoa.size(); i++){
+                cout << this->pessoa[i].getNome() << endl;
+        }
+}
+
+template <class C> void Fila<C>::setClientes(C newPessoa){
+        pessoa.push_back(newPessoa);
+}
+
 
 class Agencia{
         public:
@@ -62,6 +114,7 @@ class Conta{
         void depositar(int gold);
         void transferir(int gold, Conta* userY);
 };
+
 int Conta::num_contas = 0;
 Conta::Conta(){
         this->num_contas++;
@@ -81,12 +134,23 @@ void Conta::transferir(int gold, Conta* userY){
 
 int main(){
         Cliente joao, maria;
+        ClientePJ umbrella, aperture;
+        Fila<Cliente> ordemP;
+        Fila<ClientePJ> ordemJ;
         Agencia B1, B2;
         Conta X, Y;
         joao.setNome("Joao Nascimento"); joao.setCPF(696969);
         maria.setNome("Maria PedroS"); maria.setCPF(123456);
+        umbrella.setNome("Umbrella corp"); umbrella.setCNPJ(100001);
+        aperture.setNome("Aperture Science"); aperture.setCNPJ(424242);
 
-        B1.setBanco("Banco do capeta"); B1.setNUM(666);
+        ordemJ.setClientes(umbrella);
+        ordemJ.setClientes(aperture);
+        ordemP.setClientes(joao);
+        ordemP.setClientes(maria);
+        ordemP.getClientes();
+        ordemJ.getClientes();
+        /*B1.setBanco("Banco do capeta"); B1.setNUM(666);
         B2.setBanco("Banco divino"); B2.setNUM(999);
 
         X.AG = B1; X.titular = joao; X.saldo = 9999;
@@ -101,8 +165,8 @@ int main(){
         cout << endl;
         cout << Y.AG.getBanco() << " " << Y.AG.getNUM() << endl;
         cout << Y.titular.getNome() << " " << Y.titular.getCPF() << endl;
-        cout << "Saldo: " << Y.saldo << endl;
+        cout << "Saldo: " << Y.saldo << endl;*/
 
 
-
+        return 0;
 }
